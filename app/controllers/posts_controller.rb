91c3994e8e29a70cd98286b1a19fe7befa03707f
fredlazo <fredlazo@yahoo.com>
2016-07-20 09:10:55 -0700

    Checkpoint 34 commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,29 @@
 class PostsController < ApplicationController
-  def index
-    @posts = Post.all
-  end
+
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    @topic = Topic.find(params[:topic_id])
+    @post.topic = @topic
+
+    if @post.save
+      flash[:notice] = "Post saved successfully"
+      redirect_to [@topic,@post]
+    else
+      flash.now[:alert] = "Fail"
+      render :new
+    end
   end
 
   def edit
@@ -18,28 +37,10 @@ class PostsController < ApplicationController
 
     if @post.save
       flash[:notice] = "Post was updated successfully."
-      redirect_to @post
+      redirect_to [@post.topic, @post]
     else
       flash.now[:alert] = "There was an error saving the post. Please try again."
       render :edit
-    end
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new
-    @post.title = params[:post][:title]
-    @post.body = params[:post][:body]
-
-    if @post.save
-      flash[:notice] = "Post saved successfully"
-      redirect_to @post
-    else
-      flash.now[:alert] = "Fail"
-      render :new
     end
   end
 
@@ -48,7 +49,7 @@ class PostsController < ApplicationController
 
     if @post.destroy
       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
-      redirect_to posts_path
+      redirect_to @post.topic
     else
       flash.now[:alert] = "There was an error deleting the post."
       render :show
